fd2c3cb3f61c541d804c74ea27e6b532b8f6551a
xzc6969 <hoangquy8259747@gmail.com>
2023-10-15 20:46:34 +0700

    add user

--- src/pages/Film/Users/AddUser/AddUser.jsx
+++ src/pages/Film/Users/AddUser/AddUser.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Button,
   Cascader,
@@ -14,54 +14,64 @@ import {
 import { useDispatch } from "react-redux";
 import { useFormik } from "formik";
 import { userService } from "../../../../services/user";
+import * as Yup from "yup";
+
 const AddUser = () => {
   const [componentSize, setComponentSize] = useState("default");
   const dispatch = useDispatch();
+  const addUser = Yup.object().shape({
+    taiKhoan:Yup.string().required("Tài khoản không được để trống!"),
+    matKhau:Yup.string().required("Mật khẩu không được để trống!"),
+    email:Yup.string().required("Email không được để trống!").matches(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"Vui lòng nhập đúng định dạng!"),
+    soDt:Yup.string().required("Giá vé không được để trống!").matches(/^[0-9]+$/,"Vui lòng nhậ đúng định dạng!"),
+    hoTen: Yup.string().required("Họ tên không được để trống!").matches(/^[ aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcCdDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊjJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢpPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ]+$/,"Vui lòng nhập đúng định dạng!"),
+    maLoaiNguoiDung:Yup.string().required("Loại người dùng không được để trống!"),
+
+  })
+  const formik = useFormik({
+    initialValues: {
+      taiKhoan: "",
+      matKhau: "",
+      email: "",
+      soDt: "",
+      hoTen: "",
+      maNhom: "GP01",
+      maLoaiNguoiDung: "",
+    },
+    validationSchema:addUser,
+    onSubmit: async (values) => {
+      console.log(values);
+      try {
+        const result = await userService.fetchAdduserApi(values);
+        alert("Thêm người dùng thành công!");
+      } catch (errors) {
+        console.log(errors.response?.data);
+      }
+    },
+  });
   const [userInfo, setUserInfo] = useState({
-    taiKhoan: "",
-    matKhau: "",
-    email: "",
-    soDt: "",
-    hoTen: "",
-    maNhom: "GP01",
     maLoaiNguoiDung: [],
   });
-  const maLoaiNguoiDung = async()=>{
-    try{
-        const result = await userService.maLoaiNguoiDungApi();
-        setUserInfo({...userInfo, maLoaiNguoiDung:result.data.content});
-        console.log(result.data.content);
-    }
-    catch (errors) {
-        console.log("errors", errors);
-      }
-  }
-  const handleChange = (event) => {
-    console.log(event.target.value);
-    setUserInfo({
-      // dynamic thông qua object literals
-      [event.target.name]: event.target.value,
-    });
+  const handleChangeNguoiDung = async (value) => {
+    formik.setFieldValue("maLoaiNguoiDung", value);
   };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch(addUser(userInfo));
-  };
-  const addUser = async (state) => {
+  useEffect(() => {
+    maLoaiNguoiDung();
+  }, []);
+  const maLoaiNguoiDung = async () => {
     try {
-      const result = await userService.fetchAdduserApi(state);
-      alert("Thêm người dùng thành công!");
-      console.log(result.data.content);
+      const result = await userService.maLoaiNguoiDungApi();
+      setUserInfo({ ...userInfo, maLoaiNguoiDung: result.data.content });
     } catch (errors) {
-      console.log(errors.response?.data);
+      console.log("errors", errors);
     }
   };
+  
   const onFormLayoutChange = ({ size }) => {
     setComponentSize(size);
   };
   return (
     <Form
-      onSubmitCapture={handleSubmit}
       labelCol={{
         span: 4,
       }}
@@ -77,29 +87,53 @@ const AddUser = () => {
       style={{
         maxWidth: 600,
       }}
+      onSubmitCapture={formik.handleSubmit}
+
     >
       <Form.Item label="Tài khoản">
-        <Input onChange={handleChange} name="taiKhoan"/>
+        <Input name="taiKhoan" onChange={formik.handleChange} />
+        {formik.errors.taiKhoan && formik.touched.taiKhoan && (
+            <span className='form-label text-danger'>{formik.errors.taiKhoan}</span>
+          )}
       </Form.Item>
       <Form.Item label="Mật khẩu">
-        <Input onChange={handleChange} name="matKhau"/>
+        <Input name="matKhau" onChange={formik.handleChange} />
+        {formik.errors.matKhau && formik.touched.matKhau && (
+            <span className='form-label text-danger'>{formik.errors.matKhau}</span>
+          )}
       </Form.Item>
       <Form.Item label="Họ tên">
-        <Input onChange={handleChange} name="hoTen" />
+        <Input name="hoTen" onChange={formik.handleChange} />
+        {formik.errors.hoTen && formik.touched.hoTen && (
+            <span className='form-label text-danger'>{formik.errors.hoTen}</span>
+          )}
       </Form.Item>
       <Form.Item label="Email">
-        <Input onChange={handleChange} name="email"/>
+        <Input name="email" onChange={formik.handleChange} />
+        {formik.errors.email && formik.touched.email && (
+            <span className='form-label text-danger'>{formik.errors.email}</span>
+          )}
       </Form.Item>
       <Form.Item label="Số ĐT">
-        <InputNumber onChange={handleChange} name="soDt"/>
+        <Input name="soDt" onChange={formik.handleChange} />
+        {formik.errors.soDt && formik.touched.soDt && (
+            <span className='form-label text-danger'>{formik.errors.soDt}</span>
+          )}
       </Form.Item>
       <Form.Item label="Loại người dùng">
-        <Select>
-          
-        </Select>
+        <Select
+          options={userInfo.maLoaiNguoiDung?.map((mlnd, idx) => ({
+            label: mlnd.tenLoai,
+            value: mlnd.maLoaiNguoiDung,
+          }))}
+          onChange={handleChangeNguoiDung}
+          placeholder="Chọn loại khách hàng"
+        />
+         {formik.errors.maLoaiNguoiDung && formik.touched.maLoaiNguoiDung && (
+            <span className='form-label text-danger'>{formik.errors.maLoaiNguoiDung}</span>
+          )}
       </Form.Item>
-
-      <button className="btn btn-success mr-2">Thêm người dùng</button>
+      <Button htmlType="submit">Thêm người dùng</Button>
     </Form>
   );
 };
